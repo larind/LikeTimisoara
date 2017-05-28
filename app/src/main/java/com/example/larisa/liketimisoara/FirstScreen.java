41175f40772e93a53d26c3c73aa37136e21e0d4e
Larisa Indries <larisa.indries@yahoo.com>
2017-05-29 00:07:24 +0300

    add tranlate libraby and add more attractions

--- app/src/main/java/com/example/larisa/liketimisoara/FirstScreen.java
+++ app/src/main/java/com/example/larisa/liketimisoara/FirstScreen.java
@@ -5,14 +5,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
+import com.akexorcist.localizationactivity.LocalizationActivity;
 import com.example.larisa.liketimisoara.activity.MainActivity;
 import com.example.larisa.liketimisoara.activity.MapsActivity;
 
-public class FirstScreen extends AppCompatActivity {
+public class FirstScreen extends LocalizationActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_screen);
 
@@ -23,5 +25,24 @@ public class FirstScreen extends AppCompatActivity {
                 startActivity(mainIntent);
             }
         });
+
+        ImageButton roButton = (ImageButton) findViewById(R.id.ro_flag);
+        ImageButton enButton = (ImageButton) findViewById(R.id.en_flag);
+
+        roButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setLanguage("ro");
+            }
+        });
+
+        enButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setLanguage("en");
+            }
+        });
+
+
     }
 }
